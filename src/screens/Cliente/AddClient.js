df633e1f050ec218f0a3d4801bd53da22419b07e
UrielEC <107601993+UrielEC@users.noreply.github.com>
2022-07-28 02:23:50 -0500

    Squashed commit of the following:
    commit 8a2cfa0350fbddc53b17f74e43484634e3bc1f7c
    Author: Victor <chesterfield154@hotmail.com>
    Date:   Thu Jul 28 02:21:19 2022 -0500
    
        mapa
    
        mapa
    
    commit 2c3eeca2c89607c58cdc08cca2bf3e66900bcc90
    Author: Victor <chesterfield154@hotmail.com>
    Date:   Thu Jul 28 02:12:08 2022 -0500
    
        Mapa
    
        Se agrego mapa

--- src/screens/Cliente/AddClient.js
+++ src/screens/Cliente/AddClient.js
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet,Text, View } from 'react-native';
 import MyButton from '../../components/MyButton';
 import MyInput from '../../components/MyInput';
 import { globalStyles } from '../../styles/global';
 import { useDispatch } from 'react-redux';
 import { signIn } from '../../features/auth/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import React from 'react';
+import MapView from 'react-native-maps';
 
 export default function AddClient({ navigation }) {
 
@@ -15,7 +17,19 @@ export default function AddClient({ navigation }) {
     <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>Nombre de la tienda</Text>
       <MyInput  label="Nombre de tienda" />
+      <MapView style={styles.map}/>
        <MyButton title="Registrar" onPress={() => submitLogin } />
     </View>
   );
 }
+
+
+
+
+const styles = StyleSheet.create({
+  //Estilo del Mapa
+  map: {
+    width: '100%',
+    height: '100%'
+  }
+});
